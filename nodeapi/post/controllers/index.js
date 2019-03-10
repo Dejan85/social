fd1b722bc454f1904bb50e8d069eac51c48b593c
unknown <xypnotica@gmail.com>
2019-03-11 00:28:37 +0100

    xad

--- nodeapi/post/controllers/index.js
+++ nodeapi/post/controllers/index.js
@@ -1,6 +1,22 @@
 const Post = require("../models/post");
 const formidable = require("formidable");
 const fs = require("fs");
+const _ = require("lodash");
+
+// make post by id
+exports.postById = (req, res, next, id) => {
+  Post.findById(id)
+    .populate("postedBy", "_id name")
+    .exec((err, post) => {
+      if (err || !post) {
+        return res.status(400).json({
+          error: err
+        });
+      }
+      req.post = post;
+      next();
+    });
+};
 
 // get post
 exports.getPost = (req, res) => {
@@ -60,4 +76,34 @@ exports.postsByUser = (req, res) => {
       }
       res.json(posts);
     });
+};
+
+// update post
+exports.updatePost = (req, res, next) => {
+  let post = req.post;
+  post = _.extend(post, req.body);
+  post.updated = Date.now();
+  post.save(err => {
+    if (err) {
+      return res.status(400).json({
+        error: err
+      });
+    }
+    res.json(post);
+  });
+};
+
+// delete post
+exports.deletePost = (req, res) => {
+  let post = req.post;
+  post.remove((err, post) => {
+    if (err) {
+      return res.status(400).json({
+        error: err
+      });
+    }
+    res.json({
+      message: "Post deleted successfully"
+    });
+  });
 };
--- nodeapi/post/routes/index.js
+++ nodeapi/post/routes/index.js
@@ -2,11 +2,19 @@ const express = require("express");
 const router = express.Router();
 
 const { createPostValidator } = require("../validator");
-const { getPost, createPost, postsByUser } = require("../controllers");
+const {
+  postById,
+  getPost,
+  createPost,
+  postsByUser,
+  deletePost,
+  updatePost
+} = require("../controllers");
+const { isPoster } = require("../authorization");
 const { userById } = require("../../user/controllers");
 const { requireSignin } = require("../../login/autorization");
 
-router.get("/get", requireSignin, getPost);
+router.get("/post", requireSignin, getPost);
 router.post(
   "/post/new/:userId",
   requireSignin,
@@ -14,8 +22,12 @@ router.post(
   createPostValidator
 );
 router.get("/posts/by/:userId", postsByUser);
+router.put("/post/:postId", requireSignin, isPoster, updatePost);
+router.delete("/post/:postId", requireSignin, isPoster, deletePost);
 
 // any route containing :userId, our app will first execute userById()
 router.param("userId", userById);
+// any route containing :postById, our app will first execute postById()
+router.param("postId", postById);
 
 module.exports = router;
