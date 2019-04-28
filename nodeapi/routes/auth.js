bbd1c6a9edc0aa946c56cb2b246b4bbe4d121fae
unknown <xypnotica@gmail.com>
2019-04-28 19:04:37 +0200

    add halpers method

--- nodeapi/routes/auth.js
+++ nodeapi/routes/auth.js
@@ -7,9 +7,16 @@ const { userSignupValidator } = require("../validator");
 // controllers
 const { signup, signin, signout } = require('../controllers/auth');
 
+// halpers
+const { userById } = require('../halpers');
+
+// route
 router.post('/singup', userSignupValidator, signup);
 router.post('/signin', signin);
 router.get('/signout', signout);
+
+// param
+router.param('userId', userById); // any route containing :userId, our app will first execute userById()
 
 
 module.exports = router;
