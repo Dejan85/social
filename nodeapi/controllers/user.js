5d9333bd996d277f75edb99990f8c8c9adafc373
unknown <xypnotica@gmail.com>
2019-04-28 21:47:36 +0200

    update user done

--- nodeapi/controllers/user.js
+++ nodeapi/controllers/user.js
@@ -1,4 +1,6 @@
 const User = require('../models/user');
+const _ = require('lodash');
+
 
 // get all users
 exports.allUsers = (req, res) => {
@@ -21,4 +23,28 @@ exports.getUser = (req, res) => {
     req.profile.salt = undefined;
     req.profile.hashed_password = undefined;
     return res.json(req.profile);
-}
+};
+
+// edit user
+exports.updateUser = (req, res) => {
+    let user = req.profile;
+    user = _.extend(user, req.body); // extend - mutate the source object
+    user.updated = Date.now();
+    user.save((err) => {
+        console.log('radi');
+
+        if (err) {
+            return res.status(400).json({
+                error: "You are not authorized to preform this action"
+            });
+        };
+
+        user.salt = undefined;
+        user.hashed_password = undefined;
+
+        console.log(user);
+        res.json({
+            user
+        });
+    });
+};
--- nodeapi/routes/user.js
+++ nodeapi/routes/user.js
@@ -8,11 +8,12 @@ const { userById } = require('../halpers');
 const { requireSignin } = require('../authorization')
 
 // controllers
-const { allUsers, getUser } = require('../controllers/user');
+const { allUsers, getUser, updateUser } = require('../controllers/user');
 
 // route
 router.get('/user', allUsers)
 router.get('/user/:userId', requireSignin, getUser)
+router.put('/user/:userId', requireSignin, updateUser)
 
 // param
 router.param('userId', userById); // any route containing :userId, our app will first execute userById()
