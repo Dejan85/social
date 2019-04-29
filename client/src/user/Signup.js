21003157d23e778cd1bf51ba2c87dcbd51f814f6
unknown <xypnotica@gmail.com>
2019-04-29 16:41:29 +0200

    signup form done

--- client/src/user/Signup.js
+++ client/src/user/Signup.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
 
 class Signup extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            name: "",
+            email: "",
+            passwrod: "",
+            error: ""
+        }
+    }
     render() {
         return (
-            <div>
-                <h2>Signup</h2>
+            <div className="container">
+                <h2 className="mt-5 mb-5">Signup</h2>
+                <form>
+                    <div className="form-group">
+                        <label className="text-muted">Name</label>
+                        <input className="form-control" type="text" />
+                    </div>
+                    <div className="form-group">
+                        <label className="text-muted">Email</label>
+                        <input className="form-control" type="text" />
+                    </div>
+                    <div className="form-group">
+                        <label className="text-muted">Password</label>
+                        <input className="form-control" type="password" />
+                    </div>
+                    <button className="btn btn-raised btn-primary">Submit</button>
+                </form>
             </div>
         )
     }
