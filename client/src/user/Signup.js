60454ce180ab766862991e9ff8175fbf45f51711
unknown <xypnotica@gmail.com>
2019-04-29 16:52:35 +0200

    handling onChange events done

--- client/src/user/Signup.js
+++ client/src/user/Signup.js
@@ -7,26 +7,35 @@ class Signup extends Component {
         this.state = {
             name: "",
             email: "",
-            passwrod: "",
+            password: "",
             error: ""
         }
     }
+
+    handleChange = (name) => (e) => {
+        this.setState({
+            [name]: e.target.value
+        });
+    };
+
+
     render() {
+        const { name, email, password } = this.state;
         return (
             <div className="container">
                 <h2 className="mt-5 mb-5">Signup</h2>
                 <form>
                     <div className="form-group">
                         <label className="text-muted">Name</label>
-                        <input className="form-control" type="text" />
+                        <input className="form-control" type="text" onChange={this.handleChange("name")} value={name} />
                     </div>
                     <div className="form-group">
                         <label className="text-muted">Email</label>
-                        <input className="form-control" type="text" />
+                        <input className="form-control" type="text" onChange={this.handleChange("email")} value={email} />
                     </div>
                     <div className="form-group">
                         <label className="text-muted">Password</label>
-                        <input className="form-control" type="password" />
+                        <input className="form-control" type="password" onChange={this.handleChange("password")} value={password} />
                     </div>
                     <button className="btn btn-raised btn-primary">Submit</button>
                 </form>
