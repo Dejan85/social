661ee0d63696d36e3680ab1bb67a480bdd985966
unknown <xypnotica@gmail.com>
2019-05-01 02:43:06 +0200

    showing user info from local storage

--- client/src/user/Profile.js
+++ client/src/user/Profile.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
+import { isAuthenticated } from '../auth';
 
 class Profile extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            user: "",
+            redirectToSignin: false
+        };
+    };
+
+
+
+    componentDidMount() {
+        console.log(this.props.match.params.userId);
+    }
+
     render() {
         return (
             <div className="container">
                 <h2 className="mt-5 mb-5">Profile</h2>
+                <p>Hello {isAuthenticated().user.name}</p>
+                <p>Email: {isAuthenticated().user.email}</p>
             </div>
-        )
+        );
     };
 };
 
