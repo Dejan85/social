19f8e51d3b6d16dbbf1544244b663e71a025b6f0
unknown <xypnotica@gmail.com>
2019-05-01 14:34:23 +0200

    users component

---
+++ client/src/user/Users.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Users extends Component {
+    render() {
+        return (
+            <div className="container">
+                <h2 className="mt-5 mb-5">
+                    User
+                </h2>
+            </div>
+        )
+    }
+}
+
+export default Users;
