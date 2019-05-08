796fd971faccd7b06da5ba944cbc0fd635e3504e
unknown <xypnotica@gmail.com>
2019-05-09 01:24:41 +0200

    node api post image

--- nodeapi/controllers/post.js
+++ nodeapi/controllers/post.js
@@ -91,3 +91,9 @@ exports.deletePost = (req, res) => {
     });
   });
 };
+
+// get photo
+exports.postPhoto = (req, res, next) => {
+  res.set('Content-Type', req.post.photo.contentType);
+  return res.send(req.post.photo.data);
+};
--- nodeapi/routes/post.js
+++ nodeapi/routes/post.js
@@ -11,18 +11,30 @@ const { requireSignin, isPoster } = require('../authorization');
 const { userById, postById } = require('../halpers');
 
 // controllers
-const { getPosts, createPost, postsByUser, deletePost, updatePost } = require('../controllers/post');
-
+const {
+  getPosts,
+  createPost,
+  postsByUser,
+  deletePost,
+  updatePost,
+  postPhoto
+} = require('../controllers/post');
 
 router.get('/posts', getPosts);
-router.post('/post/new/:userId', requireSignin, createPost, createPostValidator);
+router.post(
+  '/post/new/:userId',
+  requireSignin,
+  createPost,
+  createPostValidator
+);
 router.get('/post/by/:userId', requireSignin, postsByUser);
 router.put('/post/:postId', requireSignin, isPoster, updatePost);
 router.delete('/post/:postId', requireSignin, isPoster, deletePost);
-
+// photo
+router.get('/user/photo/:postId', postPhoto);
 
 // param
 router.param('userId', userById); // any route containing :userId, our app will first execute userById()
-router.param("postId", postById); // any route containing :postById, our app will first execute postById()
+router.param('postId', postById); // any route containing :postById, our app will first execute postById()
 
 module.exports = router;
