7b98e8f3ba73cef124962633f22c07e7c56c0307
unknown <xypnotica@gmail.com>
2019-05-09 19:35:18 +0200

    React frontend - Like Unlike methods

--- client/src/post/apiPost.js
+++ client/src/post/apiPost.js
@@ -89,3 +89,21 @@ export const update = (postId, token, post) => {
       console.log(err);
     });
 };
+
+export const like = (userId, token, postId) => {
+  return fetch(`http://localhost:8080/post/unlike`, {
+    method: 'PUT',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`
+    },
+    body: JSON.stringify({ userId, postId })
+  })
+    .then(response => {
+      return response.json();
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
